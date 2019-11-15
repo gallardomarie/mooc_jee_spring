99e4ef7d61ac893ea1cb0d5fdcf44ec165bc6d92
Gallardo Marie <gallardo@b11p7.fil.univ-lille1.fr>
2019-11-15 17:16:23 +0100

    MG - Rendu de la partie Transmettre des paramètres

--- week1/exo103/WEB-INF/classes/web/BagServlet.java
+++ week1/exo103/WEB-INF/classes/web/BagServlet.java
@@ -6,33 +6,56 @@ import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.*;
 import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
 
 // TODO: this class should extends something to be a usable servlet.
 // TODO: add an annotation here to map your servlet on an URL.
-public class BagServlet {
-	
+@WebServlet(urlPatterns={"/bag"})
+public class BagServlet extends HttpServlet {
+
 	Bag myBag = new Bag();
-	
-	protected void doGet(HttpServletRequest req, HttpServletResponse res) 
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse res)
 	throws ServletException, IOException {
+		res.setContentType( "text/html" );
 		PrintWriter out = res.getWriter();
-		
-		// TODO : print a html form using printwriter.
+		out.println("<html><body>");
+		out.println("<form action='bag' method='post'>");
+		out.println("Référence <input name='ref'><p>");
+		out.println("Quantity <input name='qty'><p>");
+		out.println("<input type='submit' value='Valider'/>");
+		out.println("</form>");
+		out.println("</body></html>");
 	}
 
-	
-	
-	protected void doPost(HttpServletRequest req, HttpServletResponse res) 
+
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse res)
 	throws ServletException, IOException {
 		PrintWriter out = res.getWriter();
-		
+		res.setContentType( "text/html" );
+		String ref = req.getParameter("ref");
+		String qty = req.getParameter("qty");
+
+		if((ref==null) || (qty==null) || ref.equals("") || qty.equals("")){
+			out.println("<html><body>");
+			out.println("Une erreur a été rencontrée !!");
+			out.println("</body></html>");
+			res.setStatus(400);
+		}
+		else{
+			out.println("<html><body>");
+			out.println("<p>"+ref+"</p>");
+			out.println("<p>"+qty+"</p>");
+			out.println("</body></html>");
+		}
 		// TODO : Get parameters, check null
-		
+
 		// TODO : print reference and quantity
 
 	}
-	
-	
-	
+
+
+
 
 }
